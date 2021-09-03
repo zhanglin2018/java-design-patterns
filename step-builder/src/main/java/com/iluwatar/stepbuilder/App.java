e74c126dc8f93d94d6e0230e4a004d6d1585b9f7
Laughing Zhang <lin.zhang@advantest.com>
2021-09-03 15:20:36 +0800

    modify the stepBuilder package.

--- step-builder/src/main/java/com/iluwatar/stepbuilder/App.java
+++ step-builder/src/main/java/com/iluwatar/stepbuilder/App.java
@@ -28,72 +28,62 @@ import lombok.extern.slf4j.Slf4j;
 /**
  * Step Builder Pattern
  *
- * <p><b>Intent</b> <br>
- * An extension of the Builder pattern that fully guides the user through the creation of the object
- * with no chances of confusion. <br> The user experience will be much more improved by the fact
- * that he will only see the next step methods available, NO build method until is the right time to
- * build the object.
+ * <p>
+ * <b>Intent</b> <br>
+ * An extension of the Builder pattern that fully guides the user through the
+ * creation of the object with no chances of confusion. <br>
+ * The user experience will be much more improved by the fact that he will only
+ * see the next step methods available, NO build method until is the right time
+ * to build the object.
  *
- * <p><b>Implementation</b> <br>
+ * <p>
+ * <b>Implementation</b> <br>
  * The concept is simple:
  * <ul>
  *
- * <li>Write creational steps inner classes or interfaces where each method knows what can be
- * displayed next.</li>
+ * <li>Write creational steps inner classes or interfaces where each method
+ * knows what can be displayed next.</li>
  *
  * <li>Implement all your steps interfaces in an inner static class.</li>
  *
- * <li>Last step is the BuildStep, in charge of creating the object you need to build.</li>
+ * <li>Last step is the BuildStep, in charge of creating the object you need to
+ * build.</li>
  * </ul>
  *
- * <p><b>Applicability</b> <br>
- * Use the Step Builder pattern when the algorithm for creating a complex object should be
- * independent of the parts that make up the object and how they're assembled the construction
- * process must allow different representations for the object that's constructed when in the
- * process of constructing the order is important.
- * <br>
+ * <p>
+ * <b>Applicability</b> <br>
+ * Use the Step Builder pattern when the algorithm for creating a complex object
+ * should be independent of the parts that make up the object and how they're
+ * assembled the construction process must allow different representations for
+ * the object that's constructed when in the process of constructing the order
+ * is important. <br>
  *
- * @see <a href="http://rdafbn.blogspot.co.uk/2012/07/step-builder-pattern_28.html">http://rdafbn.blogspot.co.uk/2012/07/step-builder-pattern_28.html</a>
+ * @see <a href=
+ *      "http://rdafbn.blogspot.co.uk/2012/07/step-builder-pattern_28.html">http://rdafbn.blogspot.co.uk/2012/07/step-builder-pattern_28.html</a>
  */
 @Slf4j
 public class App {
+	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(App.class);
 
-  /**
-   * Program entry point.
-   *
-   * @param args command line args
-   */
-  public static void main(String[] args) {
+	/**
+	 * Program entry point.
+	 *
+	 * @param args command line args
+	 */
+	public static void main(String[] args) {
 
-    var warrior = CharacterStepBuilder
-        .newBuilder()
-        .name("Amberjill")
-        .fighterClass("Paladin")
-        .withWeapon("Sword")
-        .noAbilities()
-        .build();
+		var warrior = CharacterStepBuilder.newBuilder().name("Amberjill").fighterClass("Paladin").withWeapon("Sword")
+				.noAbilities().build();
 
-    LOGGER.info(warrior.toString());
+		log.info(warrior.toString());
 
-    var mage = CharacterStepBuilder
-        .newBuilder()
-        .name("Riobard")
-        .wizardClass("Sorcerer")
-        .withSpell("Fireball")
-        .withAbility("Fire Aura")
-        .withAbility("Teleport")
-        .noMoreAbilities()
-        .build();
+		var mage = CharacterStepBuilder.newBuilder().name("Riobard").wizardClass("Sorcerer").withSpell("Fireball")
+				.withAbility("Fire Aura").withAbility("Teleport").noMoreAbilities().build();
 
-    LOGGER.info(mage.toString());
+		log.info(mage.toString());
 
-    var thief = CharacterStepBuilder
-        .newBuilder()
-        .name("Desmond")
-        .fighterClass("Rogue")
-        .noWeapon()
-        .build();
-
-    LOGGER.info(thief.toString());
-  }
+		var thief = CharacterStepBuilder.newBuilder().name("Desmond").fighterClass("Rogue").noWeapon().build();
+		
+		log.info(thief.toString());
+	}
 }
