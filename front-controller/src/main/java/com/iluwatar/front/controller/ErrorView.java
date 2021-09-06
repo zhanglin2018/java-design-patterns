c9403bb7260da3be0726e13ac08f0fbdb1933955
Laughing Zhang <lin.zhang@advantest.com>
2021-09-06 17:53:36 +0800

    modify front controller

--- front-controller/src/main/java/com/iluwatar/front/controller/ErrorView.java
+++ front-controller/src/main/java/com/iluwatar/front/controller/ErrorView.java
@@ -31,8 +31,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ErrorView implements View {
 
-  @Override
-  public void display() {
-    LOGGER.error("Error 500");
-  }
+	@Override
+	public void display() {
+		LOGGER.error("Error 500");
+	}
 }
