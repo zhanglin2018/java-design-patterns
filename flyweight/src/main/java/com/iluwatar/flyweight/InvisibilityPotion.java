f67f93dc3c67b542ba66994d8b7f2c20722b78ed
Laughing Zhang <lin.zhang@advantest.com>
2021-09-06 17:23:20 +0800

    modify flyweight

--- flyweight/src/main/java/com/iluwatar/flyweight/InvisibilityPotion.java
+++ flyweight/src/main/java/com/iluwatar/flyweight/InvisibilityPotion.java
@@ -31,8 +31,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class InvisibilityPotion implements Potion {
 
-  @Override
-  public void drink() {
-    LOGGER.info("You become invisible. (Potion={})", System.identityHashCode(this));
-  }
+	@Override
+	public void drink() {
+		LOGGER.info("You become invisible. (Potion={})", System.identityHashCode(this));
+	}
 }
